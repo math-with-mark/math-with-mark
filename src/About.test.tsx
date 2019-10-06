1659f3eda0b4e1db6e50fd322f999703e9aac51d
Mark Wiemer <markwiemer@outlook.com>
2019-10-05 23:04:05 -0500

    DO NOT MERGE

--- src/About.test.tsx
+++ src/About.test.tsx
@@ -6,4 +6,5 @@ import About from './About';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<About />, div);
+  expect(true).toBe(false); // introduced failure to test Circle CI
 });
