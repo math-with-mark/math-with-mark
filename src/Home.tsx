7de6fa3b8ddfe8412fee9bb08de9fc0387852d46
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2019-10-05 23:00:28 -0500

    Link to repository from homepage (#10)

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,6 +4,13 @@ const Home = (): JSX.Element => (
   <div>
     <h2>Home</h2>
     <p>This site doesn't exist yet, unfortunately. But I'm working on it!</p>
+    <p>
+      Check out{' '}
+      <a href="https://github.com/mark-wiemer/math-with-mark">
+        the repository on GitHub
+      </a>{' '}
+      for more information.
+    </p>
   </div>
 );
 
