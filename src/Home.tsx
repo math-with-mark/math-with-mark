d5956954810febdba890f5c55063718b2e747f0d
Mark Wiemer <markwiemer@outlook.com>
2019-10-10 08:59:21 -0500

    Add MathJax

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = (): JSX.Element => (
   <div>
     <h2>Home</h2>
-    <p>This site doesn't exist yet, unfortunately. But I'm working on it!</p>
+    <p>\[a^2 + b^2 = c^2\]</p>
+    <p>Free, accessible, interactive math lessons.</p>
+    <p>This site is under construction.</p>
     <p>
       Check out{' '}
       <a href="https://github.com/mark-wiemer/math-with-mark">
