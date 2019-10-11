975f4065a41ea99e61a4c7d774b95a7359f03b44
Mark Wiemer <markwiemer@outlook.com>
2019-10-11 18:52:06 -0500

    Ensure MathJax renders always with react-MathJax

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import MathJax from 'react-mathjax';
 
 const Home = (): JSX.Element => (
   <div>
     <h2>Home</h2>
-    <p>\[a^2 + b^2 = c^2\]</p>
     <p>Free, accessible, interactive math lessons.</p>
     <p>This site is under construction.</p>
     <p>
@@ -13,6 +13,16 @@ const Home = (): JSX.Element => (
       </a>{' '}
       for more information.
     </p>
+    <MathJax.Provider>
+      <p>
+        This is an inline math formula:{' '}
+        <MathJax.Node inline formula={'a^2 + b^2 = c^2'} />
+      </p>
+      <p>
+        And a block one:
+        <MathJax.Node formula={'F(x) = \\int_a^b f(t)\\,dt'} />
+      </p>
+    </MathJax.Provider>
   </div>
 );
 
