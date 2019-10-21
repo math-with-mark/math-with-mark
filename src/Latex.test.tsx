59abb88ec448e61381f424fc673d45f1af8439be
Mark Wiemer <markwiemer@outlook.com>
2019-10-21 15:45:54 -0500

    Fix typo in latex test

--- src/Latex.test.tsx
+++ src/Latex.test.tsx
@@ -6,7 +6,7 @@ it('should render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <>
-      <Latex inline content="a^ + b^2 = c^2" />
+      <Latex inline content="a^2 + b^2 = c^2" />
       <Latex content="a^2 + b^2 = c^2" />
     </>,
     div,
