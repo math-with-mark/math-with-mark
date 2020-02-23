c3e61cc33b3fb624abac0b214f3469a3d5d3c3cd
Mark Wiemer <markwiemer@outlook.com>
2020-02-22 19:45:13 -0600

    Add stateful editable field example into app top

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { HashRouter } from 'react-router-dom';
 import ConstructionAlert from './ConstructionAlert';
 import Navigation from './Navigation';
 import Routes, { routes } from './Routes';
+import { EditableMathExample } from './pages/Sandbox';
 
 const App = (): JSX.Element => (
   <div>
+    <EditableMathExample />
     <ConstructionAlert />
     <HashRouter>
       <Navigation routes={routes} />
--- src/pages/Sandbox.tsx
+++ src/pages/Sandbox.tsx
@@ -1,9 +1,74 @@
 import React from 'react';
 
-import { addStyles, EditableMathField } from 'react-mathquill';
+import { addStyles, EditableMathField, MathField } from 'react-mathquill';
 
 addStyles();
 
-const Sandbox = () => <EditableMathField latex="a^2+b^2=c^2" />;
+interface State {
+  latex: string
+}
 
-export default Sandbox;
+const Sandbox = (props?: State): JSX.Element => (
+    <EditableMathField
+    latex={props ? props.latex : ''}
+    onChange={mathField => {
+      console.log(mathField.latex())
+    }}
+    />
+)
+
+export default Sandbox
+
+
+const initialLatex =
+  '\\cos\\left(A\\right)=\\frac{b^2+c^2-a^2}{2\\cdot b\\cdot c}'
+
+export class EditableMathExample extends React.Component<any, any> {
+  mathQuillEl: null | MathField;
+  resetField: () => void;
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      latex: initialLatex,
+      text: 'cos(A)=(b^2+c^2-a^2)/(2*b*c)',
+    }
+
+    this.mathQuillEl = null
+
+    this.resetField = () => {
+      (this.mathQuillEl as MathField).latex(initialLatex)
+    }
+  }
+
+  render() {
+    console.log(this)
+    return (
+      <div>
+        Math field:{' '}
+        <EditableMathField
+          latex={this.state.latex}
+          onChange={mathField => {
+            const latex = mathField.latex()
+            const text = (mathField as any).text()
+            console.log('latex changed:', latex)
+            console.log('text changed:', text)
+            this.setState({ latex, text })
+          }}
+          mathquillDidMount={el => {
+            this.mathQuillEl = el
+          }}
+        />
+        <div className="result-container">
+          <span>Raw latex:</span>
+          <span className="result-latex">{this.state.latex}</span>
+        </div>
+        <div className="result-container">
+          <span>Raw text:</span>
+          <span className="result-latex">{this.state.text}</span>
+        </div>
+        <button onClick={this.resetField}>Reset field</button>
+      </div>
+    )
+  }
+}
