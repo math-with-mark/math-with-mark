67e60ba727348e0b000778c84871ab05868b23f0
Mark Wiemer <markwiemer@outlook.com>
2020-10-21 22:22:33 -0700

    Remove old functionality
    We're starting over.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 
 import ConstructionAlert from './ConstructionAlert';
-import Navigation from './Navigation';
-import Routes, { routes } from './Routes';
 
 const App = (): JSX.Element => (
   <div>
     <ConstructionAlert />
-    <HashRouter>
-      <Navigation routes={routes} />
-      <Routes routes={routes} />
-    </HashRouter>
   </div>
 );
 
--- src/pages/Sandbox.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Sandbox from './Sandbox';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Sandbox />, div);
-});
