3f99084309cfae781e6f92309441fca5647dc5ee
Mark Wiemer <markwiemer@outlook.com>
2023-02-20 04:02:51 -0800

    Add basic mathjs to my-app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
+import * as mathjs from 'mathjs';
 import { MathJaxContext } from 'better-react-mathjax';
 import MathJax from 'better-react-mathjax/MathJax';
 import { EditableMathField, addStyles } from 'react-mathquill';
 
 addStyles();
+
+export function texToMath(tex: string): string {
+  tex = tex.split('{').join('(');
+  tex = tex.split('}').join(')');
+  tex = tex.split('\\left').join('');
+  tex = tex.split('\\right').join('');
+  tex = tex.split(/\\cdot ?/).join(' * ');
+  return tex;
+}
+
+export function tryParse(mathText: string) {
+  if (mathText.trim() === '') return null;
+  try {
+    let node = mathjs.parse(mathText);
+    return node;
+  } catch (err) {
+    return null;
+  }
+}
 
 function App() {
   const [latex, setLatex] = React.useState('\\frac{1}{\\sqrt{2}}\\cdot 2');
@@ -15,6 +35,7 @@ function App() {
         onChange={(mathField) => setLatex(mathField.latex())}
       />
       <MathJax>{`$$${latex}$$`}</MathJax>
+      <p>{`Parsed LaTeX: ${tryParse(texToMath(latex))?.toString()}`}</p>
     </MathJaxContext>
   );
 }
