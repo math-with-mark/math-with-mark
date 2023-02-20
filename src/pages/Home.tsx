4b68eff2cbc2a571533bf88c1c5f7571e1b61afb
Mark Wiemer <markwiemer@outlook.com>
2023-02-20 04:31:43 -0800

    Revert "Remove old functionality"
    This reverts commit 67e60ba727348e0b000778c84871ab05868b23f0.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = (): JSX.Element => (
+  <div>
+    <h2>Home</h2>
+    <p>Free, accessible, interactive math lessons.</p>
+    <p>This site is under construction.</p>
+    <p>
+      Check out{' '}
+      <a href="https://github.com/mark-wiemer/math-with-mark">
+        the repository on GitHub
+      </a>{' '}
+      for more information.
+    </p>
+    <p>Last updated 2020-10-01 00:31 PDT</p>
+  </div>
+);
+
+export default Home;
---
+++ src/pages/Sandbox.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { addStyles, EditableMathField, MathField } from 'react-mathquill';
+
+import * as mathwm from '../ts/mathwm';
+import * as rules from '../ts/rules';
+import Latex from '../Latex';
+
+addStyles();
+
+/** Wraps StatefulSandbox for procedurally-generated routing */
+const Sandbox = (): JSX.Element => <StatefulSandbox />;
+
+class StatefulSandbox extends React.Component<any, any> {
+  mathField: MathField;
+  constructor(props?: any) {
+    super(props);
+    this.state = {
+      latex: '', // used for initial population
+    };
+    this.mathField = (null as unknown) as MathField; // assigned on mount
+  }
+
+  onChange = (): void => {
+    this.updateState();
+  };
+
+  mathQuillDidMount = (mathField: MathField): void => {
+    this.mathField = mathField;
+    this.updateState();
+  };
+
+  updateState(): void {
+    const latex = this.mathField.latex();
+    const text = this.mathField.text();
+    const mathText = mathwm.texToMath(latex);
+    const node = mathwm.tryParse(mathText);
+    const evaluation =
+      node === null
+        ? 'Invalid expression'
+        : mathwm.evaluate(node, false).toString();
+    super.setState({ latex, text, evaluation });
+  }
+
+  evaluationSteps = (): JSX.Element => {
+    const mathText = mathwm.texToMath(this.state.latex);
+    const node = mathwm.tryParse(mathText);
+    if (node === null) return <p>Invalid expression</p>;
+    const steps = mathwm.steps(node);
+    return (
+      <div>
+        <p>Evaluation steps:</p>
+        <ol>{steps.map(this.step)}</ol>
+      </div>
+    );
+  };
+
+  step = (s: mathwm.Step): JSX.Element => {
+    let rule: rules.Rule = rules.RULES[s.ruleID];
+    return (
+      <li key={s.node.toString()}>
+        {s.node.toString()} ({rule.name}
+        {rule.latex ? (
+          <>
+            : <Latex inline content={rule.latex}></Latex>
+          </>
+        ) : (
+          <></>
+        )}
+        )
+      </li>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        Enter an expression:{' '}
+        <EditableMathField
+          latex={this.state.latex}
+          onChange={this.onChange}
+          mathquillDidMount={this.mathQuillDidMount}
+        />
+        <br />
+        <div>(Unprocessed) Tex: {this.state.latex}</div>
+        <br />
+        <div>(Processed) MathText: {mathwm.texToMath(this.state.latex)}</div>
+        <br />
+        <div>Evaluation of MathText: {this.state.evaluation}</div>
+        <br />
+        {this.evaluationSteps()}
+      </div>
+    );
+  }
+}
+
+export default Sandbox;
