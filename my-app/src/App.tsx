6e14ac6debbb03a9a95e934d9dbc699b41ba4a8f
Mark Wiemer <markwiemer@outlook.com>
2023-02-20 02:49:43 -0800

    Add better-react-mathjax

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,22 @@
+import { MathJaxContext } from 'better-react-mathjax';
+import MathJax from 'better-react-mathjax/MathJax';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MathJaxContext>
+      <MathJax>{'\\(\\frac{10}{4x} \\approx 2^{12}\\)'}</MathJax>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </MathJaxContext>
   );
 }
 
