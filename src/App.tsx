8a868dd6b56f98515406d6af2d43ac3b842b80cd
Mark Wiemer <markwiemer@outlook.com>
2023-04-10 22:33:14 -0700

    Add KaTeX for comparison

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { EditableMathField } from 'react-mathquill';
 import Sandbox from './Sandbox';
 import { texToMath, tryParse } from './utils/mathwm';
+import TeX from '@matejmazur/react-katex';
 
 function App() {
   const [latex, setLatex] = React.useState('1+2x+3y+4x+5y+6');
@@ -10,11 +11,17 @@ function App() {
   return (
     <>
       <h1>Math with Mark</h1>
+      <h2>Math editor</h2>
       <EditableMathField
         latex={latex}
         onChange={(mathField) => setLatex(mathField.latex())}
       />
-      <MathJax dynamic>{`$$${latex}$$`}</MathJax>
+      <p>
+        MathJax: <MathJax dynamic>{`$$${latex}$$`}</MathJax>
+      </p>
+      <p>
+        KaTeX: <TeX block math={latex} />
+      </p>
       <p>{`Parsed LaTeX: ${tryParse(texToMath(latex))?.toString()}`}</p>
       <h2>Sandbox</h2>
       <Sandbox />
