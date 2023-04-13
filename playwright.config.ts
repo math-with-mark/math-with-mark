b4160f9f8179f48eb2b5cd2dc79b606e79966f1e
Mark Wiemer <markwiemer@outlook.com>
2023-04-13 00:13:40 -0700

    Cleanup Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,36 +12,25 @@ const devicesToUse: (keyof typeof devices)[] = [
 const baseUrl = 'http://localhost:3000';
 
 export default defineConfig({
-  // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: 'src',
-
-  // Run all tests in parallel.
   fullyParallel: true,
-
-  // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI,
+  reporter: 'html',
+  use: { baseURL: baseUrl },
 
   // Opt out of parallel tests on CI.
   workers: process.env.CI ? 1 : undefined,
 
-  // Reporter to use
-  reporter: 'html',
-
-  use: {
-    // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: baseUrl,
-  },
-
+  // Each device must be in its own project
   projects: devicesToUse.map((deviceKey) => ({
     name: deviceKey as string,
-    use: { ...devices[deviceKey] },
+    use: devices[deviceKey],
     testMatch: /.+\.e2e\.ts/,
   })),
 
-  // Run your local dev server before starting the tests.
+  // Serve build to handle issues that may only appear in built environment
+  // e.g. https://github.com/fast-reflexes/better-react-mathjax/issues/42
   webServer: {
-    // Serve build to handle issues that may only appear in built environment
-    // e.g. https://github.com/fast-reflexes/better-react-mathjax/issues/42
     command: 'npm run serve-build',
     url: baseUrl,
     reuseExistingServer: !process.env.CI,
