2abc5e4a1f751ec51215e5c3ed5102dbec8867ed
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2023-04-13 01:21:18 -0700

    Add e2e tests (Playwright) (#70)

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const devicesToUse: (keyof typeof devices)[] = [
+  'Desktop Chrome',
+  'Desktop Edge',
+  'Desktop Firefox',
+  'Pixel 2',
+];
+
+const baseUrl = 'http://localhost:3000';
+
+export default defineConfig({
+  testDir: 'src',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  reporter: 'html',
+  use: { baseURL: baseUrl },
+
+  // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
+
+  // Each device must be in its own project
+  projects: devicesToUse.map((deviceKey) => ({
+    name: deviceKey as string,
+    use: devices[deviceKey],
+    testMatch: /.+\.e2e\.ts/,
+  })),
+
+  // Serve build to handle issues that may only appear in built environment
+  // e.g. https://github.com/fast-reflexes/better-react-mathjax/issues/42
+  webServer: {
+    command: 'npm run serve-build',
+    url: baseUrl,
+    reuseExistingServer: !process.env.CI,
+  },
+});
