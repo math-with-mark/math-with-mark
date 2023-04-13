35b8b67d98acf5a3bf58ad11145816494d710680
Mark Wiemer <markwiemer@outlook.com>
2023-04-13 00:08:56 -0700

    Add e2e tests
    Close #41

--- src/App.e2e.ts
+++ src/App.e2e.ts
@@ -2,7 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Math with Mark/);
+});
+
+test('homepage loads', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
